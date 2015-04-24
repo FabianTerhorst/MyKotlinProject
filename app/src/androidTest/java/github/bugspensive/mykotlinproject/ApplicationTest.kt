69774fb7f4d316693fb1c65c86eee7a08df0593e
fabianterhorst <fabian.terhorst@gmail.com>
2015-04-25 01:51:40 +0200

    -update ApplicationTest.java to Kotlin language

---
+++ app/src/androidTest/java/github/bugspensive/mykotlinproject/ApplicationTest.kt
@@ -0,0 +1,9 @@
+package github.bugspensive.mykotlinproject
+
+import android.app.Application
+import android.test.ApplicationTestCase
+
+/**
+ * [Testing Fundamentals](http://d.android.com/tools/testing/testing_android.html)
+ */
+public class ApplicationTest : ApplicationTestCase<Application>(javaClass<Application>())
